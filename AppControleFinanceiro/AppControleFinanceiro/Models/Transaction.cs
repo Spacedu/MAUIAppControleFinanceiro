e3bab4f29e972714175b98c66d7e8285b5cfe9a1
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-12-28 22:44:46 -0300

    S10E12 - Configurar o LiteDB e TransactionRepository

--- AppControleFinanceiro/AppControleFinanceiro/Models/Transaction.cs
+++ AppControleFinanceiro/AppControleFinanceiro/Models/Transaction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace AppControleFinanceiro.Models
 {
     public class Transaction
     {
+        [BsonId]
         public int Id { get; set; }
         public TransactionType Type { get; set; }
         public String Name { get; set; }
--- AppControleFinanceiro/AppControleFinanceiro/Repositories/TransactionRepository.cs
+++ AppControleFinanceiro/AppControleFinanceiro/Repositories/TransactionRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AppControleFinanceiro.Models;
+using LiteDB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +10,35 @@ namespace AppControleFinanceiro.Repositories
 {
     public class TransactionRepository : ITransactionRepository
     {
+        private readonly LiteDatabase _database;
+        private readonly string collectionName = "transactions";
         public TransactionRepository()
         {
+            _database = new LiteDatabase("Filename=C:/users/AppData/database.db;Connection=Shared");
         }
         public List<Transaction> GetAll()
         {
-
+            return _database
+                .GetCollection<Transaction>(collectionName)
+                .Query()
+                .OrderByDescending(a=>a.Date)
+                .ToList();
         }
         public void Add(Transaction transaction)
         {
-
+            var col = _database.GetCollection<Transaction>(collectionName);
+            col.Insert(transaction);
+            col.EnsureIndex(a => a.Date);
         }
         public void Update(Transaction transaction)
         {
-
+            var col = _database.GetCollection<Transaction>(collectionName);
+            col.Update(transaction);
         }
         public void Delete(Transaction transaction)
         {
-
+            var col = _database.GetCollection<Transaction>(collectionName);
+            col.Delete(transaction.Id);
         }
     }
 }
