bd5113d844fbbe41d59a4983f8a5b3def1aea145
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-12-28 21:55:51 -0300

    S10E10 - Criando Modelo

---
+++ AppControleFinanceiro/AppControleFinanceiro/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public TransactionType Type { get; set; }
+        public String Name { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public decimal Value { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        Income,
+        Expenses
+    }
+}
