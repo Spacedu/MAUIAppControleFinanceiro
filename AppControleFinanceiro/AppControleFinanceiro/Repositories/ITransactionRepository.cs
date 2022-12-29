45ee757fa954e6bc6d394417f503c64635fcaab9
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-12-28 22:07:19 -0300

    S10E11 - Instalar LiteDB e Criar Repository - Parte 1

---
+++ AppControleFinanceiro/AppControleFinanceiro/Repositories/ITransactionRepository.cs
@@ -0,0 +1,12 @@
+﻿using AppControleFinanceiro.Models;
+
+namespace AppControleFinanceiro.Repositories
+{
+    public interface ITransactionRepository
+    {
+        void Add(Transaction transaction);
+        void Delete(Transaction transaction);
+        List<Transaction> GetAll();
+        void Update(Transaction transaction);
+    }
+}
---
+++ AppControleFinanceiro/AppControleFinanceiro/Repositories/TransactionRepository.cs
@@ -0,0 +1,32 @@
+﻿using AppControleFinanceiro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        public TransactionRepository()
+        {
+        }
+        public List<Transaction> GetAll()
+        {
+
+        }
+        public void Add(Transaction transaction)
+        {
+
+        }
+        public void Update(Transaction transaction)
+        {
+
+        }
+        public void Delete(Transaction transaction)
+        {
+
+        }
+    }
+}
