8edaf7ecd7a8ca92b5647bac285bb407fc87e6db
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-01-01 17:48:51 -0300

    S10E37 - TransactionList - Converter para o Valor da Transação

---
+++ AppControleFinanceiro/AppControleFinanceiro/Libraries/Converters/TransactionValueColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using AppControleFinanceiro.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Libraries.Converters
+{
+    internal class TransactionValueColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Transaction transaction = (Transaction)value;
+            if (transaction == null)
+            {
+                return Colors.Black;
+            }
+            if (transaction.Type == TransactionType.Income)
+            {
+                return Color.FromArgb("#FF939E5A");
+            }
+            else
+            {
+                return Colors.Red;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppControleFinanceiro/AppControleFinanceiro/Libraries/Converters/TransactionValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using AppControleFinanceiro.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Libraries.Converters
+{
+    public class TransactionValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Transaction transaction = (Transaction)value;
+            if(transaction == null)
+            {
+                return "";
+            }
+            if(transaction.Type == TransactionType.Income)
+            {
+                return transaction.Value.ToString("C");
+            }
+            else
+            {
+                return $"- {transaction.Value.ToString("C")}";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
