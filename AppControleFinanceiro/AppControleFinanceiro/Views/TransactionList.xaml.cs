dbcd5756d47b9f8186682557aacf339d37d27f3d
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-01-01 12:52:22 -0300

    S10E31 - TransactionList - Listar Transações do Banco

--- AppControleFinanceiro/AppControleFinanceiro/Views/TransactionList.xaml.cs
+++ AppControleFinanceiro/AppControleFinanceiro/Views/TransactionList.xaml.cs
@@ -1,15 +1,21 @@
+using AppControleFinanceiro.Repositories;
+
 namespace AppControleFinanceiro.Views;
 
 public partial class TransactionList : ContentPage
 {
 	private TransactionAdd _transactionAdd;
 	private TransactionEdit _transactionEdit;
-    public TransactionList(TransactionAdd transactionAdd, TransactionEdit transactionEdit)
+	private ITransactionRepository _repository;
+    public TransactionList(TransactionAdd transactionAdd, TransactionEdit transactionEdit, ITransactionRepository repository)
 	{
 		this._transactionAdd = transactionAdd;
 		this._transactionEdit = transactionEdit;
+		this._repository = repository;
 
 		InitializeComponent();
+
+        CollectionViewTransactions.ItemsSource = _repository.GetAll();
 	}
 
 	private void OnButtonClicked_To_TransactionAdd(object sender, EventArgs args)
