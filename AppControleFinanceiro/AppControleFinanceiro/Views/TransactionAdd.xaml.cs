0984773d237ef1348762896e8461741a548ca046
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-12-31 21:45:28 -0300

    S10E27 - Validação.

--- AppControleFinanceiro/AppControleFinanceiro/Views/TransactionAdd.xaml.cs
+++ AppControleFinanceiro/AppControleFinanceiro/Views/TransactionAdd.xaml.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace AppControleFinanceiro.Views;
 
 public partial class TransactionAdd : ContentPage
@@ -10,5 +12,42 @@ public partial class TransactionAdd : ContentPage
     private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
 		Navigation.PopModalAsync();
+    }
+
+    private void OnButtonClicked_Save(object sender, EventArgs e)
+    {
+        //TODO - Validar os dados digitados (Pegar as informações).
+        //TODO - Salvar no Banco
+        //TODO - Fechar a Tela*
+    }
+
+    private bool IsValidData()
+    {
+        bool valid = true;
+        StringBuilder sb = new StringBuilder();
+
+        if(string.IsNullOrEmpty(EntryName.Text) || string.IsNullOrWhiteSpace(EntryName.Text))
+        {
+            sb.AppendLine("O campo 'Nome' deve ser preenchido!");
+            valid = false;
+        }
+        if (string.IsNullOrEmpty(EntryValue.Text) || string.IsNullOrWhiteSpace(EntryValue.Text))
+        {
+            sb.AppendLine("O campo 'Valor' deve ser preenchido!");
+            valid = false;
+        }
+        double result;
+        if (string.IsNullOrEmpty(EntryValue.Text) && !double.TryParse(EntryValue.Text, out result))
+        {
+            sb.AppendLine("O campo 'Valor' é inválido!");
+            valid = false;
+        }
+
+
+        if(valid == false)
+        {
+            LabelError.Text = sb.ToString();
+        }
+        return valid;
     }
 }
