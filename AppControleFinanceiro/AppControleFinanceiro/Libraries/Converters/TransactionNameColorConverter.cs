6d25f3872181bb06cd774f3b7aa32cae40e7588d
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-01-02 13:20:25 -0300

    S10E39 - TransactionList - Circulo com Cores diferentes e Letra inicial do Nome da Transação

---
+++ AppControleFinanceiro/AppControleFinanceiro/Libraries/Converters/TransactionNameColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Libraries.Converters
+{
+    public class TransactionNameColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Color.FromArgb("#FFFFFF");
+            
+            var random = new Random();
+            var color = String.Format("#FF{0:X6}", random.Next(0x1000000));
+            return Color.FromArgb(color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppControleFinanceiro/AppControleFinanceiro/Libraries/Converters/TransactionNameConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Libraries.Converters
+{
+    public class TransactionNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            String transactionName = (String)value;
+
+            return transactionName.ToUpper()[0];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
