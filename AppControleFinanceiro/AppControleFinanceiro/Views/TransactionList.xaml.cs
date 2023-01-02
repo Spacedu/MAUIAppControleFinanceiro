0988518d755a3066655ae069f501cfa1129db95c
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2023-01-02 11:44:09 -0300

    S10E38 - TransactionList - Excluir Transação

--- AppControleFinanceiro/AppControleFinanceiro/Views/TransactionList.xaml.cs
+++ AppControleFinanceiro/AppControleFinanceiro/Views/TransactionList.xaml.cs
@@ -57,4 +57,17 @@ public partial class TransactionList : ContentPage
 		transactionEdit.SetTransactionToEdit(transaction);
         Navigation.PushModalAsync(transactionEdit);
     }
+
+    private async void TapGestureRecognizerTapped_ToDelete(object sender, TappedEventArgs e)
+    {
+		bool result = await App.Current.MainPage.DisplayAlert("Excluir!","Tem certeza que deseja excluir?","Sim", "Não");
+
+		if (result)
+		{
+			Transaction transaction = (Transaction)e.Parameter;
+			_repository.Delete(transaction);
+
+			Reload();
+		}
+    }
 }
